b44fd2b646fd8176f848dfaf402b0f582c01ea7c
HumaCode <humaidizakaria1998@gmail.com>
2025-02-12 23:38:53 +0700

    frontend panel - mastering login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,233 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+    <title>Login Page</title>
+    <link rel="icon" type="{{ asset('/') }}frontend/assets/image/png"
+        href="{{ asset('/') }}frontend/assets/images/favicon.png">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/animated_barfiller.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/slick.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/venobox.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/scroll_button.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/nice-select.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/pointer.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/jquery.calendar.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/range_slider.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/startRating.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/video_player.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/jquery.simple-bar-graph.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/select2.min.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/sticky_menu.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/animate.css">
+
+    <link rel=" stylesheet" href="{{ asset('/') }}frontend/assets/css/spacing.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/style.css">
+    <link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/responsive.css">
+
+    @vite(['resources/css/frontend.css'])
+
+</head>
+
+<body class="home_3">
+
+
+    <!--============ PRELOADER START ===========-->
+    <div id="preloader">
+        <div class="preloader_icon">
+            <img src="{{ asset('/') }}frontend/assets/images/preloader.png" alt="Preloader" class="img-fluid">
+        </div>
+    </div>
+    <!--============ PRELOADER START ===========-->
+
+
+    <!--===========================
+        SIGN IN START
+    ============================-->
+    <section class="wsus__sign_in">
+        <div class="row align-items-center">
+            <div class="col-xxl-5 col-xl-6 col-lg-6 wow fadeInLeft">
+                <div class="wsus__sign_img">
+                    <img src="{{ asset('/') }}frontend/assets/images/login_img_1.jpg" alt="login"
+                        class="img-fluid">
+                    <a href="index.html">
+                        <img src="{{ asset('/') }}frontend/assets/images/logo.png" alt="EduCore" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-9 m-auto wow fadeInRight">
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+
+
+                <div class="wsus__sign_form_area">
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                            aria-labelledby="pills-home-tab" tabindex="0">
+
+                            <form action="{{ route('login') }}" method="POST">
+                                @csrf
+
+                                <h2>Log in<span>!</span></h2>
+                                <p class="new_user">Welcome back, please provide your credentials for login
+                                </p>
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label for="email">Email*</label>
+                                            <input type="email" name="email" id="email" autofocus
+                                                placeholder="Email" value="{{ old('email') }}">
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label for="password">Password*
+                                                @if (Route::has('password.request'))
+                                                    <a href="{{ route('password.request') }}">Forgot Password?</a>
+                                                @endif
+                                            </label>
+                                            <input type="password" name="password" id="password"
+                                                placeholder="Password">
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value=""
+                                                    id="flexCheckDefault" name="remember">
+                                                <label class="form-check-label" for="flexCheckDefault">
+                                                    Remember Me
+                                                </label>
+                                            </div>
+                                            <button type="submit" class="common_btn">Sign In</button>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </form>
+
+                            <p class="create_account">Don't have an account? <a href="{{ route('register') }}">Create
+                                    free
+                                    account</a></p>
+                        </div>
+                        <div class="tab-pane fade" id="pills-profile" role="tabpanel"
+                            aria-labelledby="pills-profile-tab" tabindex="0">
+                            <form action="#">
+                                <h2>Log in<span>!</span></h2>
+                                <p class="new_user">New User ? <a href="sign_up.html">Create an Account</a></p>
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label>Email or Username*</label>
+                                            <input type="text" placeholder="Email or Username">
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label>Email or Username* <a href="#">Forgot Password?</a></label>
+                                            <input type="password" placeholder="Password">
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value=""
+                                                    id="flexCheckDefault1">
+                                                <label class="form-check-label" for="flexCheckDefault1">
+                                                    Remember Me
+                                                </label>
+                                            </div>
+                                            <button type="submit" class="common_btn">Sign In</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <p class="create_account">Don't have an account? <a href="sign_up.html">Create free
+                                    account</a></p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <a class="back_btn" href="{{ route('home') }}">Back to Home</a>
+    </section>
+    <!--===========================
+        SIGN IN END
+    ============================-->
+
+
+    <!--================================
+        SCROLL BUTTON START
+    =================================-->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+    <!--================================
+        SCROLL BUTTON END
+    =================================-->
+
+
+    <!--jquery library js-->
+    <script src="{{ asset('/') }}frontend/assets/js/jquery-3.7.1.min.js"></script>
+    <!--bootstrap js-->
+    <script src="{{ asset('/') }}frontend/assets/js/bootstrap.bundle.min.js"></script>
+    <!--font-awesome js-->
+    <script src="{{ asset('/') }}frontend/assets/js/Font-Awesome.js"></script>
+    <!--marquee js-->
+    <script src="{{ asset('/') }}frontend/assets/js/jquery.marquee.min.js"></script>
+    <!--slick js-->
+    <script src="{{ asset('/') }}frontend/assets/js/slick.min.js"></script>
+    <!--countup js-->
+    <script src="{{ asset('/') }}frontend/assets/js/jquery.waypoints.min.js"></script>
+    <script src="{{ asset('/') }}frontend/assets/js/jquery.countup.min.js"></script>
+    <!--venobox js-->
+    <script src="{{ asset('/') }}frontend/assets/js/venobox.min.js"></script>
+    <!--nice-select js-->
+    <script src="{{ asset('/') }}frontend/assets/js/jquery.nice-select.min.js"></script>
+    <!--Scroll Button js-->
+    <script src="{{ asset('/') }}frontend/assets/js/scroll_button.js"></script>
+    <!--pointer js-->
+    <script src="{{ asset('/') }}frontend/assets/js/pointer.js"></script>
+    <!--range slider js-->
+    <script src="{{ asset('/') }}frontend/assets/js/range_slider.js"></script>
+    <!--barfiller js-->
+    <script src="{{ asset('/') }}frontend/assets/js/animated_barfiller.js"></script>
+    <!--calendar js-->
+    <script src="{{ asset('/') }}frontend/assets/js/jquery.calendar.js"></script>
+    <!--starRating js-->
+    <script src="{{ asset('/') }}frontend/assets/js/starRating.js"></script>
+    <!--Bar Graph js-->
+    <script src="{{ asset('/') }}frontend/assets/js/jquery.simple-bar-graph.min.js"></script>
+    <!--select2 js-->
+    <script src="{{ asset('/') }}frontend/assets/js/select2.min.js"></script>
+    <!--Video player js-->
+    <script src="{{ asset('/') }}frontend/assets/js/video_player.min.js"></script>
+    <script src="{{ asset('/') }}frontend/assets/js/video_player_youtube.js"></script>
+    <!--wow js-->
+    <script src="{{ asset('/') }}frontend/assets/js/wow.min.js"></script>
+
+    <!--main/custom js-->
+    <script src="{{ asset('/') }}frontend/assets/js/main.js"></script>
+
+</body>
+
+</html>
