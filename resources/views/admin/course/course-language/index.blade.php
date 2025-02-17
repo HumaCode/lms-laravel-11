6148223c752591f06435d3ba6c1a9156bab2e470
HumaCode <humaidizakaria1998@gmail.com>
2025-02-17 23:08:40 +0700

    Course Language - Working with Delete Feature (Part -1)

--- resources/views/admin/course/course-language/index.blade.php
+++ resources/views/admin/course/course-language/index.blade.php
@@ -95,7 +95,8 @@
                                                                     </svg>
                                                                 </a>
 
-                                                                <a href="" class="btn-sm text-red">
+                                                                <a href="{{ route('admin.course-languages.destroy', $language->id) }}"
+                                                                    class="btn-sm text-red delete-item">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24"
                                                                         height="24" viewBox="0 0 24 24" fill="none"
                                                                         stroke="currentColor" stroke-width="2"
